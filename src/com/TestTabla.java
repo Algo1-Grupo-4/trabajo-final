a8c0a794080dbd8d02d0e476ed8ef5370f4457bb
Lucia Vazquez <luchi08vazquez@gmail.com>
2023-10-26 12:25:13 -0300

    removeColumna y removeFila

--- src/com/TestTabla.java
+++ src/com/TestTabla.java
@@ -4,19 +4,22 @@ import java.io.IOException;
 
 public class TestTabla {
     public static void main(String[] args) throws IOException {
-        // Especifico tipos de dato
-        //String[] tiposDato = {"Boolean", "Boolean"};
-        //String[] tiposDato = {"String", "String"};
-        //String[] tiposDato = {"Number", "Number"};
-        String[] tiposDato = {"String", "Number", "Boolean"};
+        // links luchi, agregar los de ustedes abajo 
+            // Especifico tipos de dato
+            //String[] tiposDato = {"Boolean", "Boolean"};
+            //String[] tiposDato = {"String", "String"};
+            //String[] tiposDato = {"Number", "Number"};
+            String[] tiposDato = {"String", "Number", "Boolean"};
 
-        // Ruta del archivo
-        //String fileName = "C:\\Users\\Usuario\\Downloads\\df_booleanos.csv";
-        //String fileName = "C:\\Users\\Usuario\\Downloads\\df_string.csv";
-        //String fileName = "C:\\Users\\Usuario\\Downloads\\df_number.csv";
-        String fileName = "C:\\Users\\Usuario\\Downloads\\df_prueba.csv";
+            // Ruta del archivo
+            //String fileName = "C:\\Users\\Usuario\\Downloads\\df_booleanos.csv";
+            //String fileName = "C:\\Users\\Usuario\\Downloads\\df_string.csv";
+            //String fileName = "C:\\Users\\Usuario\\Downloads\\df_number.csv";
+            String fileName = "C:\\Users\\Usuario\\Downloads\\df_prueba.csv";
 
         Tabla tabla = new Tabla(tiposDato, fileName, true);
         tabla.mostrarTabla();
+
+
     }
 }   
